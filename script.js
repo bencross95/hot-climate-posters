f144eb1ecd7cb7fbbd8aa41e755b186b8121f6d1
Ben Cross <ben_cross@me.com>
2019-12-01 14:08:26 +0000

    marquee painfully done

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("js works");
+function makeMarquee () {
+  const title = 'worlds first vegan website'
+
+  const marqueeText = new Array(200).fill(title).join(' ')
+
+  const marquee = document.querySelector('.marquee span')
+
+  marquee.innerHTML = marqueeText
+
+}
+
+
+makeMarquee()
