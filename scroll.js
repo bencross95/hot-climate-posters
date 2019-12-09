642768656acc221662684f8e154c77ee230bd22f
Ben Cross <ben_cross@me.com>
2019-12-09 23:04:45 +0000

    poster scroll fixed

--- scroll.js
+++ scroll.js
@@ -1,8 +1,4 @@
-if ($(window).width() < 1400) {
-
-}
-else {
-
+if ($(window).width() > 1400) {
   $(document).on("scroll", function () {
 
     var pixels = $(document).scrollTop()
@@ -11,5 +7,29 @@ else {
 
   })
 
+}
+
+
+else {
+
+  $(document).on("scroll", function () {
+
+    var pixels = $(document).scrollTop()
+
+    $(".sideways-scroll-div").css("left", -1 * pixels + $(window).width()*0.5)
+
+  })
+
 
 }
+
+
+// if above 1400, scroll indent is 750px
+// if below, then scroll indent is 50vw
+
+
+
+
+
+
+// want the scroll indent to be 750px before 1000px window, then 50vw
